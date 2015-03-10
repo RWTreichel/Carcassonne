d25d18d47b9f8712042c3c85fdf03623cfb07d2a
divensje <justindivens@gmail.com>
2015-03-10 01:19:27 -0400

    Placement Upgrades
    - **Non-working** Example of why I need to make a branch for development
    - Added if statements to PlaceTile() to check if space is taken, then
    checks if tiles around it match (road to road, city to city).
    - Added rotate "algorithms" to transform TILE_CONFIG string accordingly.
    Based on degrees right now (0, 90, 180, 270). Could be any string
    easily.
    -More figure/plotting/turn simulation testing/junk

--- Carcassonne.py
+++ Carcassonne.py
@@ -11,9 +11,23 @@ import matplotlib.image as mpimg
 
 #Setup board
 def StartGame():
+    #Setup Board
     board = pnd.DataFrame(columns=['TILE_ID','TILE_X','TILE_Y','TILE_CONFIG','PLAYER_ID'])
+    start_append = pnd.DataFrame(columns=['TILE_ID','TILE_X','TILE_Y','TILE_CONFIG','PLAYER_ID'])
+    start_append['TILE_ID'] = 0
+    start_append['TILE_X'] = 0
+    start_append['TILE_Y'] = 0
+    start_append['TILE_CONFIG'] = 'CRFR'
+    start_append['PLAYER_ID'] = 'START'
+    board = board.append(start_append, ignore_index=True) #starting tile
+    print(start_append)
+    print(board)
+    #Setup Tiles    
     tiles = pnd.read_csv('tiles_start.csv',header=0) #import csv starter file
     tiles.insert(3,'STATUS',1) #insert status column in tiles dataframe
+    status = np.array(tiles['STATUS']) #create status array from tiles
+    status[0] = 0 #set status=0 for starting tile
+    tiles['STATUS'] = status #update status column of tiles dataframe
     return(board, tiles)
 
 #Choose random tile from pile and update status column for chosen tile
@@ -30,14 +44,45 @@ def GetTile(tiles):
     return(tiles, tile_inhand)
     
 #Update Board
-def PlaceTile(tiles, tile_inhand, board, tile_x, tile_y, tile_config, player):
-    placement = pnd.DataFrame(columns=['TILE_ID','TILE_X','TILE_Y','TILE_CONFIG'], index=[0])
+def PlaceTile(tiles, tile_inhand, board, tile_x, tile_y, rotate, player):
+    #Create dataframe object of current placement to be appended to board
+    placement = pnd.DataFrame(columns=['TILE_ID','TILE_X','TILE_Y','TILE_CONFIG','PLAYER_ID'], index=[0])
     placement['TILE_ID'] = tile_inhand
     placement['TILE_X'] = tile_x
     placement['TILE_Y'] = tile_y
-    placement['TILE_CONFIG'] = tile_config
+    if rotate == '0':
+        new_config = tiles['TILE_CONFIG'][tile_inhand]
+        placement['TILE_CONFIG'] = new_config
+    elif rotate == '90':
+        new_config = tiles['TILE_CONFIG'][tile_inhand][3] + tiles['TILE_CONFIG'][tile_inhand][0] + tiles['TILE_CONFIG'][tile_inhand][1] + tiles['TILE_CONFIG'][tile_inhand][2]
+        placement['TILE_CONFIG'] = new_config
+    elif rotate == '180':
+        new_config = tiles['TILE_CONFIG'][tile_inhand][2] + tiles['TILE_CONFIG'][tile_inhand][3] + tiles['TILE_CONFIG'][tile_inhand][0] + tiles['TILE_CONFIG'][tile_inhand][1]
+        placement['TILE_CONFIG'] = new_config
+    elif rotate == '270':
+        new_config = tiles['TILE_CONFIG'][tile_inhand][1] + tiles['TILE_CONFIG'][tile_inhand][2] + tiles['TILE_CONFIG'][tile_inhand][3] + tiles['TILE_CONFIG'][tile_inhand][0]
+        placement['TILE_CONFIG'] = new_config
+    else: 
+        print('Invalid Rotate')
     placement['PLAYER_ID'] = player
-    board = board.append(placement, ignore_index=True)
-    print(board)
-    return(board, tiles)
+    
+    #Check placement for invalid moves
+    check_center = board[(board['TILE_X']==tile_x) & (board['TILE_Y']==tile_y)]
+    check_top = board[(board['TILE_X']==tile_x) & (board['TILE_Y']==tile_y+1)]
+    check_right = board[(board['TILE_X']==tile_x+1) & (board['TILE_Y']==tile_y)]
+    check_bottom = board[(board['TILE_X']==tile_x) & (board['TILE_Y']==tile_y-1)]
+    check_left = board[(board['TILE_X']==tile_x-1) & (board['TILE_Y']==tile_y)]
+    
+    if check_center.empty:
+        if (str(check_top['TILE_CONFIG'])[2]==new_config[0] or check_top.empty) and \
+        (str(check_right['TILE_CONFIG'])[3]==new_config[1] or check_right.empty) and \
+        (str(check_bottom['TILE_CONFIG'])[0]==new_config[2] or check_bottom.empty) and \
+        (str(check_left['TILE_CONFIG'])[1]==new_config[3] or check_left.empty):
+        
+            board = board.append(placement, ignore_index=True)
+            print(board)
+            return(board)
+    else:
+        print('Invalid Move')
+        return(board)
 
--- TestArea.py
+++ TestArea.py
@@ -4,36 +4,41 @@ Testing area for Carcassonne.py
 import Carcassonne as carc
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import matplotlib.gridspec as gridspec
 
 #First turn simulation
 b1 = carc.StartGame()
+
 g1 = carc.GetTile(b1[1])
-p1 = carc.PlaceTile(g1[0],g1[1],b1[0],0,0,'CCRR',1)
+p1 = carc.PlaceTile(g1[0],g1[1],b1[0],int(input('Enter X location: ')),int(input('Enter Y location: ')),input('Enter Rotate (0, 90, 180, or 270): '),1)
 #Second turn
-g2 = carc.GetTile(p1[1])
-p2 = carc.PlaceTile(g2[0],g2[1],p1[0],1,0,'RFCF',2)  
+g2 = carc.GetTile(g1[0])
+p2 = carc.PlaceTile(g2[0],g2[1],p1,int(input('Enter X location: ')),int(input('Enter Y location: ')),input('Enter Rotate (0, 90, 180, or 270): '),2)  
 #Third turn
-g3 = carc.GetTile(p2[1])
-p3 = carc.PlaceTile(g3[0],g3[1],p2[0],1,1,'FFRF',3)
+g3 = carc.GetTile(g2[0])
+p3 = carc.PlaceTile(g3[0],g3[1],p2,int(input('Enter X location: ')),int(input('Enter Y location: ')),input('Enter Rotate (0, 90, 180, or 270): '),3)
 #Fourth turn
-g4 = carc.GetTile(p3[1])
-p4 = carc.PlaceTile(g4[0],g4[1],p3[0],0,-1,'FFRF',4)
+g4 = carc.GetTile(g3[0])
+p4 = carc.PlaceTile(g4[0],g4[1],p3,int(input('Enter X location: ')),int(input('Enter Y location: ')),input('Enter Rotate (0, 90, 180, or 270): '),4)
 #Fifth turn
-g5 = carc.GetTile(p4[1])
-p5 = carc.PlaceTile(g5[0],g5[1],p4[0],-1,0,'FFRF',1)
+g5 = carc.GetTile(g4[0])
+p5 = carc.PlaceTile(g5[0],g5[1],p4,int(input('Enter X location: ')),int(input('Enter Y location: ')),input('Enter Rotate (0, 90, 180, or 270): '),1)
 #Sixth turn
-g6 = carc.GetTile(p5[1])
-p6 = carc.PlaceTile(g6[0],g6[1],p5[0],0,-2,'FFRF',2)
+g6 = carc.GetTile(g5[0])
+p6 = carc.PlaceTile(g6[0],g6[1],p5,int(input('Enter X location: ')),int(input('Enter Y location: ')),input('Enter Rotate (0, 90, 180, or 270): '),2)
+
+"""
+
+plt.scatter(p6['TILE_X'],p6['TILE_Y'])
+
 
 fig1 = plt.figure(1)
-plt.scatter(p6[0]['TILE_X'],p6[0]['TILE_Y'])
+plt.scatter(p6['TILE_X'],p6['TILE_Y'])
 
 fig2 = plt.figure(2)
 
 plt.subplot(5,5,13)
 plt.axis('off')
-path1 = 'C:/Users/Justin/Box Sync/Python/Carcassonne/Graphics/Tiles/' + p1[1]['FILENAME'][g1[1]] #define image path for chosen tile
+path1 = 'C:/Users/Justin/Box Sync/Python/Carcassonne/Graphics/Tiles/' + p1['FILENAME'][g1[1]] #define image path for chosen tile
 img1 = mpimg.imread(path1) #define image object
 imgplot = plt.imshow(img1) #show image
 imgplot.axes.get_xaxis().set_visible(False)
@@ -41,7 +46,7 @@ imgplot.axes.get_yaxis().set_visible(False)
 
 plt.subplot(5,5,14)
 plt.axis('off')
-path2 = 'C:/Users/Justin/Box Sync/Python/Carcassonne/Graphics/Tiles/' + p2[1]['FILENAME'][g2[1]] #define image path for chosen tile
+path2 = 'C:/Users/Justin/Box Sync/Python/Carcassonne/Graphics/Tiles/' + p2['FILENAME'][g2[1]] #define image path for chosen tile
 img2 = mpimg.imread(path2) #define image object
 imgplot = plt.imshow(img2) #show image
 imgplot.axes.get_xaxis().set_visible(False)
@@ -49,10 +54,11 @@ imgplot.axes.get_yaxis().set_visible(False)
 
 plt.subplot(5,5,9)
 plt.axis('off')
-path3 = 'C:/Users/Justin/Box Sync/Python/Carcassonne/Graphics/Tiles/' + p3[1]['FILENAME'][g3[1]] #define image path for chosen tile
+path3 = 'C:/Users/Justin/Box Sync/Python/Carcassonne/Graphics/Tiles/' + p3['FILENAME'][g3[1]] #define image path for chosen tile
 img3 = mpimg.imread(path3) #define image object
 imgplot = plt.imshow(img3) #show image
 imgplot.axes.get_xaxis().set_visible(False)
 imgplot.axes.get_yaxis().set_visible(False)
 
-plt.savefig('board.png', bbox_inches='tight')
+#plt.savefig('board.png', bbox_inches='tight')
+"""
