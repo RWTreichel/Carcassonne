c69d00f0f883a45a2709f8c77026bb20cdbec277
divensje <justindivens@gmail.com>
2015-03-10 22:28:31 -0400

    Error Fixing, Start including meeple
    Fixed a bunch of errors, started incorporating meeple

--- Carcassonne.py
+++ Carcassonne.py
@@ -6,22 +6,13 @@ Carcassonne Game
 
 import numpy as np
 import pandas as pnd
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
-#Setup board
+#Setup game
 def StartGame():
     #Setup Board
-    board = pnd.DataFrame(columns=['TILE_ID','TILE_X','TILE_Y','TILE_CONFIG','PLAYER_ID'])
-    start_append = pnd.DataFrame(columns=['TILE_ID','TILE_X','TILE_Y','TILE_CONFIG','PLAYER_ID'])
-    start_append['TILE_ID'] = 0
-    start_append['TILE_X'] = 0
-    start_append['TILE_Y'] = 0
-    start_append['TILE_CONFIG'] = 'CRFR'
-    start_append['PLAYER_ID'] = 'START'
+    start_append = pnd.DataFrame({'TILE_ID':'0','TILE_X':'0','TILE_Y':'0','TILE_CONFIG':'CRFR','PLAYER_ID':'START''MEEPLE':'0'}, index=[0])
+    board = pnd.DataFrame(columns=['TILE_ID','TILE_X','TILE_Y','TILE_CONFIG','PLAYER_ID','MEEPLE'])
     board = board.append(start_append, ignore_index=True) #starting tile
-    print(start_append)
-    print(board)
     #Setup Tiles    
     tiles = pnd.read_csv('tiles_start.csv',header=0) #import csv starter file
     tiles.insert(3,'STATUS',1) #insert status column in tiles dataframe
@@ -37,19 +28,16 @@ def GetTile(tiles):
     status = np.array(tiles['STATUS']) #create status array from tiles
     status[tile_inhand] = 0 #set status=0 for randomly chosen tile
     tiles['STATUS'] = status #update status column of tiles dataframe   
-    path = 'C:/Users/Justin/Box Sync/Python/Carcassonne/Graphics/Tiles/' + tiles['FILENAME'][tile_inhand] #define image path for chosen tile
-    #img = mpimg.imread(path) #define image object
-    #imgplot = plt.imshow(img) #show image
-    print(tiles.values[tile_inhand])
     return(tiles, tile_inhand)
     
 #Update Board
-def PlaceTile(tiles, tile_inhand, board, tile_x, tile_y, rotate, player):
+def PlaceTile(tiles, tile_inhand, board, tile_x, tile_y, rotate, player, meeple):
     #Create dataframe object of current placement to be appended to board
-    placement = pnd.DataFrame(columns=['TILE_ID','TILE_X','TILE_Y','TILE_CONFIG','PLAYER_ID'], index=[0])
+    placement = pnd.DataFrame(columns=['TILE_ID','TILE_X','TILE_Y','TILE_CONFIG','PLAYER_ID','MEEPLE'], index=[0])
     placement['TILE_ID'] = tile_inhand
     placement['TILE_X'] = tile_x
     placement['TILE_Y'] = tile_y
+    
     if rotate == '0':
         new_config = tiles['TILE_CONFIG'][tile_inhand]
         placement['TILE_CONFIG'] = new_config
@@ -64,7 +52,9 @@ def PlaceTile(tiles, tile_inhand, board, tile_x, tile_y, rotate, player):
         placement['TILE_CONFIG'] = new_config
     else: 
         print('Invalid Rotate')
+        
     placement['PLAYER_ID'] = player
+    placement['MEEPLE'] = meeple
     
     #Check placement for invalid moves
     check_center = board[(board['TILE_X']==tile_x) & (board['TILE_Y']==tile_y)]
@@ -80,7 +70,9 @@ def PlaceTile(tiles, tile_inhand, board, tile_x, tile_y, rotate, player):
         (str(check_left['TILE_CONFIG'])[1]==new_config[3] or check_left.empty):
         
             board = board.append(placement, ignore_index=True)
-            print(board)
+            return(board)
+        else:
+            print('Tile features do not line up')
             return(board)
     else:
         print('Invalid Move')
