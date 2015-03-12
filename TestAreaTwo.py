05759b727e9d071ce50530c278d715a443910c0b
divensje <justindivens@gmail.com>
2015-03-12 00:24:33 -0400

    Error checking
    Tested and fixed all error checking in PlaceTile()

--- TestAreaTwo.py
+++ TestAreaTwo.py
@@ -12,7 +12,7 @@ tiles = start[1]
 
 #Start Loop
 turn = 1
-while turn < 5:
+while turn < 3:
     get_tile = carc.GetTile(tiles)
     tiles = get_tile[0]
     tile_inhand = get_tile[1]
@@ -20,11 +20,11 @@ while turn < 5:
     
     place_x = int(input('Enter X position: '))
     place_y = int(input('Enter Y position: '))
-    rotate = input('Enter rotation: ')
-    player = input('Enter player ID: ')
-    meeple = input('Enter meeple option: ')
+    rotate = str(input('Enter rotation: '))
+    player = str(input('Enter player ID: '))
+    meeple = str(input('Enter meeple option: '))
     place = carc.PlaceTile(tiles, tile_inhand, board, place_x, place_y, rotate, player, meeple)
     board = place
-    print(board)
+    print(place,'\n',board)
     
     turn = turn + 1
